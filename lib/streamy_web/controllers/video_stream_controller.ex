12de315c870dff9b17255b19a5ef2ecff91c3659
Jeremiah <jeremiahcrosby@gmail.com>
2022-10-26 23:26:18 -0400

    Got range request working for video streaming

--- lib/streamy_web/controllers/video_stream_controller.ex
+++ lib/streamy_web/controllers/video_stream_controller.ex
@@ -1,12 +1,37 @@
 defmodule StreamyWeb.VideoStreamController do
   use StreamyWeb, :controller
 
+  require Logger
+
   import Plug.Conn
 
   @doc """
   Proxies a local video file. Takes absolute local path as query string param.
   """
   def stream(conn, %{"path" => path}) do
-    send_file(conn, 200, path)
+    stats = File.stat!(path)
+    filesize = stats.size
+
+    req =
+      Regex.run(
+        ~r/bytes=([0-9]+)-([0-9]+)?/,
+        conn |> Plug.Conn.get_req_header("range") |> List.first()
+      )
+
+    {req_start, _} = req |> Enum.at(1) |> Integer.parse()
+    {req_end, _} = req |> Enum.at(2, filesize |> to_string) |> Integer.parse()
+
+    length = req_end - req_start + 1
+
+    Logger.debug("Serving #{path} from #{req_start} to #{req_end}")
+
+    conn
+    |> Plug.Conn.put_resp_header("Content-Type", "video/mp4")
+    |> Plug.Conn.put_resp_header("Accept-Ranges", "bytes")
+    |> Plug.Conn.put_resp_header(
+      "Content-Range",
+      "bytes #{req_start}-#{filesize - 1}/#{filesize}"
+    )
+    |> Plug.Conn.send_file(206, path, req_start, filesize - req_start)
   end
 end
