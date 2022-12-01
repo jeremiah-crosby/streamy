928bb11faf435a114e80514dcae3c9390a41df75
Jeremiah <jeremiahcrosby@gmail.com>
2022-11-30 22:21:02 -0500

    Fix error in runtime config and tests

--- config/runtime.exs
+++ config/runtime.exs
@@ -65,25 +65,29 @@ cond do
     # to start each relevant endpoint:
     #
     config :streamy, StreamyWeb.Endpoint, server: true
-    #
-    # Then you can assemble a release by calling `mix release`.
-    # See `mix help release` for more information.
 
-    # ## Configuring the mailer
-    #
-    # In production you need to configure the mailer to use a different adapter.
-    # Also, you may need to configure the Swoosh API client of your choice if you
-    # are not using SMTP. Here is an example of the configuration:
-    #
-    #     config :streamy, Streamy.Mailer,
-    #       adapter: Swoosh.Adapters.Mailgun,
-    #       api_key: System.get_env("MAILGUN_API_KEY"),
-    #       domain: System.get_env("MAILGUN_DOMAIN")
-    #
-    # For this example you need include a HTTP client required by Swoosh API client.
-    # Swoosh supports Hackney and Finch out of the box:
-    #
-    #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-    #
-    # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+  #
+  # Then you can assemble a release by calling `mix release`.
+  # See `mix help release` for more information.
+
+  # ## Configuring the mailer
+  #
+  # In production you need to configure the mailer to use a different adapter.
+  # Also, you may need to configure the Swoosh API client of your choice if you
+  # are not using SMTP. Here is an example of the configuration:
+  #
+  #     config :streamy, Streamy.Mailer,
+  #       adapter: Swoosh.Adapters.Mailgun,
+  #       api_key: System.get_env("MAILGUN_API_KEY"),
+  #       domain: System.get_env("MAILGUN_DOMAIN")
+  #
+  # For this example you need include a HTTP client required by Swoosh API client.
+  # Swoosh supports Hackney and Finch out of the box:
+  #
+  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
+  #
+  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+
+  true ->
+    :ok
 end
