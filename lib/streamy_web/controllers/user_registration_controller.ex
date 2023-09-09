41b4316f467c22ab8d86834f7ee3afed09c12ef0
Jeremiah <jeremiahcrosby@gmail.com>
2023-09-09 09:43:15 -0400

    Fix compilation errors

--- lib/streamy_web/controllers/user_registration_controller.ex
+++ lib/streamy_web/controllers/user_registration_controller.ex
@@ -7,18 +7,16 @@ defmodule StreamyWeb.UserRegistrationController do
 
   def new(conn, _params) do
     changeset = Accounts.change_user_registration(%User{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, :new, changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
-    IO.puts(File.cwd!())
-
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         {:ok, _} =
           Accounts.deliver_user_confirmation_instructions(
             user,
-            &Routes.user_confirmation_url(conn, :edit, &1)
+            &url(~p"/users/confirm/#{&1}")
           )
 
         conn
@@ -26,7 +24,7 @@ defmodule StreamyWeb.UserRegistrationController do
         |> UserAuth.log_in_user(user)
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, :new, changeset: changeset)
     end
   end
 end
